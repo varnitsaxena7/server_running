b5e58ccc80cdac186d7468622b7e4946ce1f90a1
varnitsaxena7 <saxenavarnit.code7@gmail.com>
2024-07-08 21:57:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const axios = require("axios");
+const cron = require("node-cron");
+const app = express();
+const port = 3003 || process.env.PORT;
+
+const servers = [
+  "https://insightengine.onrender.com/",
+];
+
+const pingServers = async () => {
+  for (const server of servers) {
+    try {
+      const res = await axios.get(server);
+      console.log(`Successfully pinged: ${server}`);
+    } catch (error) {
+      console.error(`Error pinging ${server}: ${error.message}`);
+    }
+  }
+};
+
+cron.schedule("*/10 * * * *", () => {
+  pingServers();
+});
+
+app.get("/", (req, res) => {
+  res.send("Keep-Alive Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Keep-Alive Server running at port:${port}`);
+});
