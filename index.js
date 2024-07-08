061a3f9ea44a1e7ac942d0ca8c9615a312f63140
varnitsaxena7 <118539746+varnitsaxena7@users.noreply.github.com>
2024-07-09 00:31:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cron = require("node-cron");
 const app = express();
-const port = 3003;
+const port = 3000;
 
 const servers = [
   "https://insightengine.onrender.com/",
